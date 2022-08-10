d3796ce65832a4fc9211df4e70e5bdc7fbe8a944
Deyoungj <emmanuelonyibor39@gmail.com>
2022-08-10 13:17:46 +0100

    initial commit

--- random_number.py
+++ random_number.py
@@ -1,15 +1,22 @@
 import random
 
-def generate_random():
+def generate_random_to_base10():
     
     binary_list = ''
     for i in range(4):
         t = random.randint(0,1)
 
         binary_list += str(t)
+    last = len(binary_list)-1
+    sum = 0
+    for i in range(0, len(binary_list)):
+        digit = int(binary_list[i])
+        exponent = last - i
+        multiplier = 2 ** exponent
+        integer = digit * multiplier
+        sum += integer
+    print('binary list: ',binary_list)
+    print('base 10: ',sum)
 
-    print(binary_list)
-    print(int(binary_list))
 
-
-generate_random()
+generate_random_to_base10()
